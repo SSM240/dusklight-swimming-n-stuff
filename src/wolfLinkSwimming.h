af8bbc45b0d688499418cfa1212e366ae04cbc1e
SSM24 <supersupermario077@gmail.com>
2026-08-10 19:25:12 -0400

    whitespace

--- src/wolfLinkSwimming.h
+++ src/wolfLinkSwimming.h
@@ -10,30 +10,31 @@
 #include "d/actor/d_a_alink.h"
 #include "types.h"
 
-namespace WolfLinkSwimming {
-    DEFINE_HOOK(&daAlink_c::procWolfSwimWait, HookWolfSwimWait);
-    DEFINE_HOOK(&daAlink_c::procWolfSwimMove, HookWolfSwimMove);
-    DEFINE_HOOK(&daAlink_c::wolfFootBgCheck, HookWolfFootBgCheck);
+namespace WolfLinkSwimming
+{
+DEFINE_HOOK(&daAlink_c::procWolfSwimWait, HookWolfSwimWait);
+DEFINE_HOOK(&daAlink_c::procWolfSwimMove, HookWolfSwimMove);
+DEFINE_HOOK(&daAlink_c::wolfFootBgCheck, HookWolfFootBgCheck);
 
-    static const float SWIM_ACCEL = 1.0f;
-    static const float MAX_SINK_SPEED = -10.0f;
-    static const float MAX_RISE_SPEED = 10.0f; // unused, for reference
-    static const s16 PITCH_ACCEL = 3000;
-    static const s16 PITCH_MAX = 9000;
-    static const s16 PITCH_MIN = -6000;
+static const float SWIM_ACCEL = 1.0f;
+static const float MAX_SINK_SPEED = -10.0f;
+static const float MAX_RISE_SPEED = 10.0f; // unused, for reference
+static const s16 PITCH_ACCEL = 3000;
+static const s16 PITCH_MAX = 9000;
+static const s16 PITCH_MIN = -6000;
 
-    static bool swimSinking;
-    static bool swimRising;
+static bool swimSinking;
+static bool swimRising;
 
-    static bool replacedState = false;
-    static u16 oldProcID;
+static bool replacedState = false;
+static u16 oldProcID;
 
-    ModResult init();
-    void doWolfLinkSwimMovement(daAlink_c* player);
-    void doWolfLinkSwimAngle(daAlink_c* player);
+ModResult init();
+void doWolfLinkSwimMovement(daAlink_c* player);
+void doWolfLinkSwimAngle(daAlink_c* player);
 
-    void replaceWolfSwimWait(ModContext*, void* args, void* retval, void*);
-    void replaceWolfSwimMove(ModContext*, void* args, void* retval, void*);
-    HookAction preWolfFootBgCheck(ModContext*, void* args, void*, void*);
-    void postWolfFootBgCheck(ModContext*, void* args, void*, void*);
+void replaceWolfSwimWait(ModContext*, void* args, void* retval, void*);
+void replaceWolfSwimMove(ModContext*, void* args, void* retval, void*);
+HookAction preWolfFootBgCheck(ModContext*, void* args, void*, void*);
+void postWolfFootBgCheck(ModContext*, void* args, void*, void*);
 }
