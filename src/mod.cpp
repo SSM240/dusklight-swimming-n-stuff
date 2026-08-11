bdca9af1762d004ef13f6ed2e9e6f3136d1fbdbd
SSM24 <supersupermario077@gmail.com>
2026-08-10 23:14:45 -0400

    remove leftover

--- src/mod.cpp
+++ src/mod.cpp
@@ -12,24 +12,6 @@ DEFINE_MOD();
 IMPORT_SERVICE(LogService, svc_log);
 IMPORT_SERVICE(HookService, svc_hook);
 
-//DEFINE_HOOK(&daAlink_c::initGravity, InitGravity);
-
-// this hook did not seem to do what i wanted it to
-
-//static HookAction on_init_gravity(ModContext*, void* args, void*, void*) {
-//    daAlink_c* player = mods::arg_ref<daAlink_c*>(args, 0);
-//    
-//    if (player->checkWolf() 
-//      && player->checkModeFlg(player->MODE_SWIMMING) 
-//      && !player->checkHeavyStateOn(TRUE, TRUE)) {
-//        f32 gravity = 0.0f;
-//        f32 max_fall_speed = 0.0f;
-//        player->setSpecialGravity(gravity, max_fall_speed, TRUE);
-//        return HOOK_SKIP_ORIGINAL;
-//    }
-//    return HOOK_CONTINUE;
-//}
-
 extern "C" {
 MOD_EXPORT ModResult mod_initialize(ModError*) {
     ModResult result = MOD_OK;
