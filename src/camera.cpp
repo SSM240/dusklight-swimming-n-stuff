262f2aa907abe4fca5f315ca08b033c6981ff460
SSM24 <supersupermario077@gmail.com>
2026-08-16 03:08:17 -0400

    get camera change disable on death working
    bonus: also re-enables camera controls fully now too

--- src/camera.cpp
+++ src/camera.cpp
@@ -7,6 +7,7 @@
 #include "mods/api.h"
 #include "d/actor/d_a_alink.h"
 #include "d/actor/d_a_player.h"
+#include "d/d_camera.h"
 #include "d/d_com_inf_game.h"
 #include "types.h"
 
@@ -14,14 +15,29 @@ ModResult Camera::init()
 {
     ModResult result;
 
-    POST_HOOK(HookProcCoDeadInit, postLinkProcCoDeadInit);
+    POST_HOOK(HookProcCoDead, postLinkProcCoDead);
+    POST_HOOK(HookCheckForceLockTarget, postCheckForceLockTarget);
 
     return MOD_OK;
 }
 
-void Camera::postLinkProcCoDeadInit(ModContext*, void* args, void*, void*) {
+void Camera::postLinkProcCoDead(ModContext*, void* args, void*, void*) {
     daAlink_c* player = mods::arg<daAlink_c*>(args, 0);
-    // undo camera change on death
+    // prevent camera from changing on death
     // todo: config
-    player->mDemo.resetDemoType();
+    player->mProcVar3.field_0x300e = 0;
+    shouldAllowCameraControl = true;
+}
+
+// very stupid hack
+// dCamera_c::checkForceLockTarget is a function that only runs during dCamera_c::Run
+// and conveniently, it is run just after it zeroes out the c-stick values when a 
+// cutscene is running
+// so we can hijack the function and run updatePad to read the c-stick values again
+void Camera::postCheckForceLockTarget(ModContext*, void* args, void*, void*) {
+    dCamera_c* camera = mods::arg<dCamera_c*>(args, 0);
+    if (shouldAllowCameraControl) {
+        camera->updatePad();
+    }
+    shouldAllowCameraControl = false;
 }
--- src/camera.h
+++ src/camera.h
@@ -5,13 +5,22 @@
 #include "mods/api.h"
 #include "d/actor/d_a_alink.h"
 #include "types.h"
+#include "d/d_camera.h"
 
 namespace Camera
 {
-DEFINE_HOOK(&daAlink_c::procCoDeadInit, HookProcCoDeadInit);
+DEFINE_HOOK(&daAlink_c::procCoDead, HookProcCoDead);
+DEFINE_HOOK(&dCamera_c::checkForceLockTarget, HookCheckForceLockTarget);
 
 ModResult init();
 
-void postLinkProcCoDeadInit(ModContext*, void* args, void*, void*);
+static bool shouldAllowCameraControl;
+
+static f32 mLastPosX;
+static f32 mLastPosY;
+static f32 mLastValue;
+
+void postLinkProcCoDead(ModContext*, void* args, void*, void*);
+void postCheckForceLockTarget(ModContext*, void* args, void*, void*);
 
 }
